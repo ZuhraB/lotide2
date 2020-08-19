eb39a416b844baa13a03201916a8f62448205bde
ZuhraB <zuhra.bakhshi.com>
2020-08-19 19:10:31 +0000

    countOnly

--- middle.js
+++ middle.js
@@ -44,3 +44,5 @@ console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]))// => [3]
 
 console.log(assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]))// => [2, 3]
 console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3,4])) // => [3, 4]
+
+
