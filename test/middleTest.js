3c3958a05b83982f4c895ece2d9bbae0eee28a6a
ZuhraB <zuhra.bakhshi.com>
2020-08-24 21:41:01 +0000

    added moca and chi

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,30 @@
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
-console.log(assertArraysEqual(middle([1]),[]))// => []
-console.log(assertArraysEqual(middle([1, 2]),[])) // => []
+describe ('#middle', () =>{
+  
+  it('should return [] for [1]', () => {
+    assert.deepEqual(middle[1], undefined)
+  })
+  it('should return [] for [1, 2', () => {
+    assert.deepEqual(middle(1, 2), undefined)
+  })
+  
+  it ('should return [2] for [1, 2, 3]', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2])
+  })
 
-console.log(assertArraysEqual(middle([1, 2, 3]),[2]))// => [2]
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]))// => [3]
+  it ('should return [2, 3] for [1, 2, 3, 4]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3])
+  })
+})
 
-console.log(assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]))// => [2, 3]
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3,4])) // => [3, 4]
+// console.log(assertArraysEqual(middle([1]),[]))// => []
+// console.log(assertArraysEqual(middle([1, 2]),[])) // => []
+
+// console.log(assertArraysEqual(middle([1, 2, 3]),[2]))// => [2]
+// console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]))// => [3]
+
+// console.log(assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]))// => [2, 3]
+// console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3,4])) // => [3, 4]
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,19 @@
 
-const tail = require('../tail');
-const assertEqual = require('../assertEqual');
-let nums = [5,6,7]
-tail([5,6,7])
-assertEqual(nums.length, 3);
-let nums1 = []
-tail([])
-assertEqual(nums1.length, 0);
+ const tail = require('../tail');
+// const assertEqual = require('../assertEqual');
+// let nums = [5,6,7]
+// tail([5,6,7])
+// assertEqual(nums.length, 3);
+// let nums1 = []
+// tail([])
+// assertEqual(nums1.length, 0);
+
+const assert = require('chai').assert;
+describe ("#tail", () => {
+  it ('should return [6,7] for [5, 6, 7]', () => {
+    assert.deepEqual(tail([5,6,7]),[6,7] )
+  })
+  it ('should return [] for []', () => {
+    assert.deepEqual(tail([]), undefined)
+  })
+})
