6415112f03aad037597489ed02f9ef6141d3e66e
ZuhraB <zuhra.bakhshi.com>
2020-08-24 18:53:46 +0000

    done

--- eqArrays.js
+++ eqArrays.js
@@ -1,4 +1,4 @@
-
+const assertEqual = require('./assertEqual');
 const eqArrays = function(arr1, arr2){
   if (arr1.length !== arr2.length) {
     return false;
@@ -10,17 +10,5 @@ const eqArrays = function(arr1, arr2){
   }
   return true
 }
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
 
-};
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true) // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false) // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true) // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false)// => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+module.exports = eqArrays;
